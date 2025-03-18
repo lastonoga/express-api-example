6a2b388200fb6f32b9237bb4583808c911199f13
Kir <5420754+lastonoga@users.noreply.github.com>
2025-03-18 14:00:34 +0400

    feat: update ENV to dotenv support

--- config/env.ts
+++ config/env.ts
@@ -1,4 +1,6 @@
 import type { StringValue } from 'ms';
+import type { BuiltinLogger } from 'express-zod-api'
+import 'dotenv/config'
 
 export const JWT_SECRET_TOKEN: string = process.env.JWT_SECRET_TOKEN ?? 'test';
 export const JWT_TOKEN_EXPIRE: StringValue = (process.env.JWT_TOKEN_EXPIRE ??
@@ -8,3 +10,4 @@ export const OPENWEATHER_API_TOKEN: string =
 export const API_URL: string = process.env.API_URL ?? 'http://localhost:8090';
 export const API_PORT: string = process.env.API_PORT ?? '1234';
 export const NODE_ENV: string = process.env.NODE_ENV ?? 'development';
+export const LOG_LEVEL: string = process.env.LOG_LEVEL ?? (NODE_ENV == 'production' ? 'info' : 'debug')
--- config/server.ts
+++ config/server.ts
@@ -2,11 +2,15 @@ import { createConfig } from 'express-zod-api';
 import ui from 'swagger-ui-express';
 import { getSwaggerDocumentation } from './swagger.js';
 import { limiter } from './rate-limiter.js';
-import { API_PORT, NODE_ENV, API_URL } from '@/config/env.js';
+import { API_PORT, NODE_ENV, API_URL, LOG_LEVEL } from '@/config/env.js';
 
 export const config = createConfig({
     http: {
         listen: API_PORT
+    },
+    logger: {
+        level: LOG_LEVEL as any, // or "warn" in production mode
+        depth: 2, 
     },
     cors: true,
     compression: true,
@@ -17,7 +21,7 @@ export const config = createConfig({
     },
     beforeRouting: ({ app, getLogger }) => {
         const logger = getLogger();
-        
+
         logger.info(`Serving the API documentation at ${API_URL}/docs`);
 
         app.use('/docs', ui.serve, ui.setup(getSwaggerDocumentation()));
